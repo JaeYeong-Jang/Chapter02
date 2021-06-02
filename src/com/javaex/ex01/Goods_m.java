852bea278423033cfd8ad2bf80788ddd25e12a09
jangjaeyeong <xkxh96@gmail.com>
2021-06-02 12:47:54 +0900

    6/02 coding (class...)

---
+++ src/com/javaex/ex01/Goods_m.java
@@ -0,0 +1,33 @@
+package com.javaex.ex01;
+
+public class Goods_m {
+
+	public static void main(String[]args) {
+	
+		Goods camera = new Goods();
+		
+		camera.name = "니콘";
+		camera.price = 400000;
+		
+		System.out.println(camera.name + ',' + camera.price);
+		
+		System.out.println("===========================");
+		
+		Goods laptop = new Goods();
+		
+		laptop.name = "LG그램";
+		laptop.price = 1000000;
+		
+		System.out.println(laptop.name + ',' + laptop.price);
+		
+		System.out.println("===========================");
+		
+		Goods cup = new Goods();
+		
+		cup.name = "머그컵";
+		cup.price = 2500;
+		
+		System.out.println(cup.name + ',' + cup.price);
+	}
+	
+}
